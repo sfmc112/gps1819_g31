8ef5b94e3041cd73b89e13e33161f0d1c0ac884e
Wallace Oliveira <wallace9655@gmail.com>
2018-11-23 11:10:49 +0000

    Added JUnit Dependencies to project
    - Fixed some imports

--- MovieTime/test/movietime/database/DatabaseManagerTest.java
+++ MovieTime/test/movietime/database/DatabaseManagerTest.java
@@ -7,9 +7,7 @@ package movietime.database;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
