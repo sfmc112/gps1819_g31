e61ce38651bf732307bc0c323073811f2a6652a7
Sarah Cunha <missntc.sf@gmail.com>
2018-11-23 11:52:21 +0000

    Added AuthenticationManager and User classes
    added skeleton for Auth Manager and method declarations

---
+++ MovieTime/src/movietime/accounts/User.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movietime.accounts;
+
+/**
+ *
+ * @author sarah
+ */
+public class User {
+    //TODO add attributes
+    //TODO add methods
+}
---
+++ MovieTime/src/movietime/authentication/AuthenticationManager.java
@@ -0,0 +1,20 @@
+package movietime.authentication;
+
+import movietime.accounts.User;
+
+public class AuthenticationManager {
+    
+    public static User authenticateUser(String user){
+        //TODO
+        return null;
+    }
+    
+    public static User createUser(String username, String firstName, String lastName){
+        //TODO
+        return null;
+    }
+    
+    public static void logoutUser(User user){
+        //TODO
+    }
+}
